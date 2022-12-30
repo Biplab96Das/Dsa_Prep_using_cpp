d2b167b440f05c94edc91a161aa93c040a9235e2
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-30 21:24:48 +0530

    Add files via upload

---
+++ Pattern0.cpp
@@ -0,0 +1,15 @@
+//* * * * *
+//* * * * *
+//* * * * *
+//* * * * *
+//* * * * *
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int i=1;i<=5;++i){//no.of lines
+      for(int j=1;j<=5;++j)//no.of columns
+         cout<<"* ";//element of each position of row-column
+    cout<<endl; }    
+  return 0;     
+}
