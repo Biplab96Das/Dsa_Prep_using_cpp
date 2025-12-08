e0e1e8f6ff3c5fac8ba69b80d8eae7b15ce1a4d0
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2025-12-08 12:42:34 +0530

    Time: 13 ms (5.36%) | Memory: 95.1 MB (79.13%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,22 +3,47 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int i=0,j=0;
         vector<int>Merged;
+        int cnt=0;
+        int n = nums1.size() + nums2.size();
+        int idx1 = n/2;
+        int idx2 = idx1-1;
+        int idx1ele=-1;
+        int idx2ele=-1;
         while(i<nums1.size() && j<nums2.size())
         {
             if(nums1[i]<nums2[j])
-               Merged.push_back(nums1[i++]);
+               {if(cnt==idx1)
+                  idx1ele=nums1[i];
+                if(cnt==idx2)
+                  idx2ele=nums1[i];  
+                cnt++;++i;}
             else
-              Merged.push_back(nums2[j++]);  
+              {if(cnt==idx1)
+                  idx1ele=nums2[j];
+                if(cnt==idx2)
+                  idx2ele=nums2[j];  
+                cnt++;++j;}  
         }
         while(i<nums1.size())
-           Merged.push_back(nums1[i++]);
+        {
+            if(cnt==idx1)
+                  idx1ele=nums1[i];
+            if(cnt==idx2)
+                  idx2ele=nums1[i];  
+            cnt++;++i; 
+        }
         while(j<nums2.size())
-           Merged.push_back(nums2[j++]);
-        int n = nums1.size() + nums2.size();
+        {
+            if(cnt==idx1)
+                  idx1ele=nums2[j];
+            if(cnt==idx2)
+                  idx2ele=nums2[j];  
+            cnt++;++j;
+        }
         cout<<"Size of Merged array : "<<n<<" == "<<Merged.size();
         if(n % 2 == 1)
-          return (double)(Merged[n/2]);
+          return (double)(idx1ele);
         else
-          return ((double)((double)Merged[n/2 - 1] + (double)Merged[n/2]) / 2);        
+          return ((double)((double)idx1ele + (double)idx2ele) / 2);        
     }
 };
