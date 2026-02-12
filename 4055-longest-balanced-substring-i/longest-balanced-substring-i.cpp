75e1c87ad0fe38011fe807d7ef9a3eaef3240923
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2026-02-12 23:38:06 +0530

    Time: 139 ms (75.30%) | Memory: 11.4 MB (94.41%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int res = 0;
+        int n = s.size();
+        vector<int>cnt(26);
+        for(int i=0;i<n;++i)
+        {
+            fill(cnt.begin(),cnt.end(),0);
+            for(int j = i;j<n;++j)
+            {
+                bool flag = true;
+                int c = s[j] - 'a';
+                cnt[c]++;
+                for(auto x : cnt)
+                {
+                    if(x>0 && x != cnt[c])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag)
+                {
+                    res = max(res, j - i + 1);
+                }
+            }
+        }
+        return res;
+    }
+};
