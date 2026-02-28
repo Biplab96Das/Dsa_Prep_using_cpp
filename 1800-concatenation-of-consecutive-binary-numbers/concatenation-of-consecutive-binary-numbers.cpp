90e868ff5586fe687bc22ce9ba824d480d19becc
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2026-02-28 23:29:30 +0530

    Time: 47 ms (55.03%) | Memory: 8.1 MB (41.61%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -3,9 +3,11 @@ public:
     int concatenatedBinary(int n) {
         int const mod = 1e9 + 7;
         long ans = 0;
+        int bit;
         for(int i=1;i<=n;++i)
-        {
-            ans = (ans << ((32 - __builtin_clz(i))) | i )% mod ;
+        {   
+            bit = floor(log2(i)) + 1;
+            ans = ((ans << bit) | i )% mod ;
         }
         return ans;
     }
