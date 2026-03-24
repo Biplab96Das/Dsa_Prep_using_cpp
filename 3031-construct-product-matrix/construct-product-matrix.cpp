abfa31981cdaf6b67e45f673b117ffc6e97d848c
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2026-03-24 20:05:10 +0530

    Time: 29 ms (84.13%) | Memory: 132 MB (80.07%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -3,18 +3,18 @@ const int mod = 12345;
 class Solution {
 public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
-        int n=grid.size(),m=grid[0].size();ll ans=1;
+        int n=grid.size(),m=grid[0].size();ll right=1;
         vector<vector<int>>p(n,vector<int>(m));
         for(int i=n-1;i>=0;--i)
           for(int j=m-1; j>=0;--j){
-             p[i][j] = ans;
-             ans = ans*grid[i][j]%mod;
+             p[i][j] = right;
+             right = right*grid[i][j]%mod;
              }     
-        ll mult = 1;
+        ll left = 1;
         for(int i=0;i<n;++i)
           for(int j=0;j<m;++j){
-             p[i][j] = p[i][j] * mult % mod;
-             mult = mult * grid[i][j] % mod;}
+             p[i][j] = p[i][j] * left % mod;
+             left = left * grid[i][j] % mod;}
         return p;          
     }
 };
